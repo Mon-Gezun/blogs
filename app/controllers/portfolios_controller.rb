4d82550f45ae7b53dbfcd983ddcfcfad2fce4d19
Raymond <raymond@philippinedev.com>
2021-06-10 19:23:16 +0800

    create and implement crud

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,49 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_items = Portfolio.new
+  end
+
+  def create
+    @portfolio_items = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to @portfolio_items, notice: "Portfolio was successfully created." }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    set_portfolio   
+  end
+
+  def update
+    set_portfolio
+    respond_to do |format|
+      if @portfolio_items.update( params.require(:portfolio).permit(:title, :subtitle,  :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio_items = Portfolio.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle,  :body)
+    end
+    
 end
