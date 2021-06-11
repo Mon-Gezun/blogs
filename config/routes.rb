88c8663da1e666ea07b18b4e5de32d9a384aebc2
Raymond <raymond@philippinedev.com>
2021-06-11 18:58:37 +0800

    custom routes portfolios resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'portfolios#index'
-
+  
   get 'blogs/index'
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
